313ba61adcc52846cba5d1b8ea5ed4bc89f46eb7
Hayden Hanson <hansonhayd3n@gmail.com>
2024-10-15 12:36:18 -0400

    feat: add TwoSum function stub and unit tests

--- TwoSum.Tests/TwoSumTests.cs
+++ TwoSum.Tests/TwoSumTests.cs
@@ -5,8 +5,68 @@ namespace TwoSum.Tests
   public class TwoSumTests
   {
     [Fact]
-    public void TestTwoSum()
+    public void TestTwoSumWithStandardInput()
     {
+      // Arrange
+      TwoSumSolver solver = new TwoSumSolver();
+
+      // Act
+      var result = solver.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+
+      // Assert
+      Assert.Equal(new int[] { 0, 1 }, result);
+    }
+
+    [Fact]
+    public void TestTwoSumWithMultipleValidPairs()
+    {
+      // Arrange
+      TwoSumSolver solver = new TwoSumSolver();
+
+      // Act
+      var result = solver.TwoSum(new int[] { 3, 2, 4 }, 6);
+
+      // Assert
+      Assert.Equal(new int[] { 1, 2 }, result);
+    }
+
+    [Fact]
+    public void TestTwoSumWithDuplicateNumbers()
+    {
+      // Arrange
+      TwoSumSolver solver = new TwoSumSolver();
+
+      // Act
+      var result = solver.TwoSum(new int[] { 3, 3 }, 6);
+
+      // Assert
+      Assert.Equal(new int[] { 0, 1 }, result);
+    }
+
+    [Fact]
+    public void TestTwoSumWithNegativeNumbers()
+    {
+      // Arrange
+      TwoSumSolver solver = new TwoSumSolver();
+
+      // Act
+      var result = solver.TwoSum(new int[] { -1, -2, -3, -4 }, -6);
+
+      // Assert
+      Assert.Equal(new int[] { 1, 3 }, result);
+    }
+
+    [Fact]
+    public void TestTwoSumWithLargeNumbers()
+    {
+      // Arrange
+      TwoSumSolver solver = new TwoSumSolver();
+
+      // Act
+      var result = solver.TwoSum(new int[] { 1000000, 500, 1000000 }, 2000000);
+
+      // Assert
+      Assert.Equal(new int[] { 0, 2 }, result);
     }
   }
 }
--- TwoSum/TwoSum.cs
+++ TwoSum/TwoSum.cs
@@ -1,6 +1,13 @@
 public class TwoSumSolver
 {
-  public void TwoSum()
+  /*
+    Given an array of integers nums and an integer target, return indices of
+    the two numbers such that they add up to the target.
+
+    You may assume that each input would have exactly one solution, and you
+    may not use the same element twice.
+  */
+  public int[] TwoSum(int[] nums, int target)
   {
     // Your implementation here...
     throw new ArgumentException("No ContainsDuplicate solution");
